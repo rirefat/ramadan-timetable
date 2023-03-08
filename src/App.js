88703155998f463e765e6f077d9f2ddaa2a5fe92
rirefat <rirefat.official@gmail.com>
2023-03-09 01:54:22 +0600

    done footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBanner from './components/TopBanner/TopBanner';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className='main-container'>
       <TopBanner></TopBanner>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            Developed By <a href="https://rirefat.netlify.app/" target="_blank">Rafiul Islam Refat</a> || 2023
+        </footer>
+    );
+};
+
+export default Footer;
