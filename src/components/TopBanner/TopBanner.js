29e2b22fd5753661764e0517cc69c4a101e6c258
rirefat <rirefat.official@gmail.com>
2023-03-09 01:01:58 +0600

    add clock to the top section

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TopBanner.css';
 
 const TopBanner = () => {
+    const [time, setTime]=useState('')
+    setInterval(() => {
+        let date = new Date();
+        let time = date.toLocaleTimeString();
+
+        setTime(time)
+    },1000);
+
     return (
         <div className='top-banner'>
             <div className="top-left">
+                <h4>{time}</h4>
                 <h1>Ramadan Kareem</h1>
                 <h3>SAHRI AND IFTER TIME SCHEDULE</h3>
-                <p>"O you who have believed, decreed upon you is fasting as it was decreed upon those before you that you may become righteous" <strong>-- Surat Al-Baqarah 2:183</strong></p>                
+                <p>"O you who have believed, decreed upon you is fasting as it was decreed upon those before you that you may become righteous" <strong>-- Surat Al-Baqarah 2:183</strong></p>
             </div>
             <div className="top-right">
                 <h1>Today's Schedule</h1>
