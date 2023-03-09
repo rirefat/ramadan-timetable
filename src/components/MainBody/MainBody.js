2ef1e78120dd6d882cb44bf21a84be5cdcaf15c3
rirefat <rirefat.official@gmail.com>
2023-03-10 01:35:18 +0600

    add location

--- src/components/MainBody/MainBody.js
+++ src/components/MainBody/MainBody.js
@@ -4,10 +4,13 @@ import MagfiratSection from '../MagfiratSection/MagfiratSection';
 import NazwatSection from '../NazwatSection/NazwatSection';
 import RahmatSection from '../RahmatSection/RahmatSection';
 import './MainBody.css';
+import locationImg from '../../img/2-location.gif';
+
+import { ImLocation2 } from 'react-icons/im';
 
 const MainBody = () => {
-    const [doa, setDoa]=useDoa();
-    
+    const [doa, setDoa] = useDoa();
+
     return (
         <div className='main-body'>
             <div className="left-section">
@@ -17,9 +20,16 @@ const MainBody = () => {
             </div>
 
             <div className="right-section">
+                <div className="area-section">
+                    <div className="location">
+                        {/* <span className="location-icon"><ImLocation2></ImLocation2></span> */}
+                        <img src={locationImg} alt="" />
+                        <h3>Dhaka</h3>
+                    </div>
+                </div>
                 <div className="doa-section">
                     {
-                        doa.map(singleDoa=><Doa key={singleDoa.serial} singleDoa={singleDoa}></Doa>)
+                        doa.map(singleDoa => <Doa key={singleDoa.serial} singleDoa={singleDoa}></Doa>)
                     }
                 </div>
             </div>
